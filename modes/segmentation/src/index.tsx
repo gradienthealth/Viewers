8295acbb1644cbac02ed75c1b2e048b061fe388d
Ouwen Huang <ouwen.huang@duke.edu>
2024-01-02 13:27:13 -0500

    Revert "Added formpanel to segmentation mode and added form change listener in studybrowser panel (#13)"
    This reverts commit 3e3c7a97acef00121d4a5bd5ccd324b53ff97763.

--- modes/segmentation/src/index.tsx
+++ modes/segmentation/src/index.tsx
@@ -22,10 +22,6 @@ const segmentation = {
   viewport: '@ohif/extension-cornerstone-dicom-seg.viewportModule.dicom-seg',
 };
 
-const gradienthealth = {
-  form: '@gradienthealth/ohif-gradienthealth-extension.panelModule.form',
-};
-
 /**
  * Just two dependencies to be able to render a viewport with panels in order
  * to make sure that the mode is working.
@@ -54,14 +50,7 @@ function modeFactory({ modeConfiguration }) {
      * Services and other resources.
      */
     onModeEnter: ({ servicesManager, extensionManager, commandsManager }) => {
-      const {
-        measurementService,
-        toolbarService,
-        toolGroupService,
-        GoogleSheetsService,
-        CropDisplayAreaService,
-        CacheAPIService,
-      } = servicesManager.services;
+      const { measurementService, toolbarService, toolGroupService } = servicesManager.services;
 
       measurementService.clearMeasurements();
 
@@ -97,9 +86,6 @@ function modeFactory({ modeConfiguration }) {
         activateTool
       ));
 
-      GoogleSheetsService.init();
-      CropDisplayAreaService.init();
-      CacheAPIService.init();
       toolbarService.init(extensionManager);
       toolbarService.addButtons(toolbarButtons);
       toolbarService.createButtonSection('primary', [
@@ -167,7 +153,7 @@ function modeFactory({ modeConfiguration }) {
             id: ohif.layout,
             props: {
               leftPanels: [ohif.leftPanel],
-              rightPanels: [segmentation.panelTool, gradienthealth.form],
+              rightPanels: [segmentation.panelTool],
               viewports: [
                 {
                   namespace: cornerstone.viewport,
