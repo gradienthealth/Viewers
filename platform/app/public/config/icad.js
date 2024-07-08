7b6c35358a5a5f7f4b86e6af8485307f067c5a8a
Adithyan Dinesh <adithyan.dinesh@trenser.com>
2024-07-08 19:42:54 +0530

    Add icad deploy script and config

--- platform/app/public/config/icad.js
+++ platform/app/public/config/icad.js
@@ -1,169 +1,208 @@
 window.config = {
-  routerBasename: "/",
+  routerBasename: '/icad-gradienthealth',
+  // whiteLabeling: {},
   extensions: [],
   modes: [],
-  customizationService: {},
-  showStudyList: !0,
+  customizationService: {
+    // Shows a custom route -access via http://localhost:3000/custom
+    // helloPage: '@ohif/extension-default.customizationModule.helloPage',
+  },
+  showStudyList: true,
+  // some windows systems have issues with more than 3 web workers
   maxNumberOfWebWorkers: 3,
-  omitQuotationForMultipartRequest: !0,
-  showLoadingIndicator: !0,
+
+  // below flag is for performance reasons, but it might not work for all servers
+  omitQuotationForMultipartRequest: true,
+  showWarningMessageForCrossOrigin: false,
+  showCPUFallbackMessage: true,
+  showLoadingIndicator: true,
+  strictZSpacingForVolumeViewport: true,
+  use16BitDataType: true,
   maxNumRequests: {
     interaction: 100,
     thumbnail: 75,
-    prefetch: 4
+    // Prefetch number is dependent on the http protocol. For http 2 or
+    // above, the number of requests can be go a lot higher.
+    prefetch: 4,
   },
-  oidc: [{
-    authority: "https://accounts.google.com",
-    client_id: "112149084621-0693qa0gtck3f9rd08qpbpuafq29r1n5.apps.googleusercontent.com",
-    redirect_uri: "/callback",
-    response_type: "id_token token",
-    scope: "email profile openid https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/cloudplatformprojects.readonly https://www.googleapis.com/auth/cloud-healthcare",
-    post_logout_redirect_uri: "/logout-redirect.html",
-    revoke_uri: "https://accounts.google.com/o/oauth2/revoke?token=",
-    automaticSilentRenew: !0,
-    revokeAccessTokenOnSignout: !0
-  }],
-  dataSources: [{
-    friendlyName: "dcmjs DICOMWeb Server",
-    namespace: "@ohif/extension-default.dataSourcesModule.dicomweb",
-    sourceName: "dicomweb",
-    configuration: {
-      name: "GCP",
-      wadoUriRoot: "https://healthcare.googleapis.com/v1/projects/icad-med/locations/us-central1/datasets/mammo/dicomStores/breast_density/dicomWeb",
-      qidoRoot: "https://healthcare.googleapis.com/v1/projects/icad-med/locations/us-central1/datasets/mammo/dicomStores/breast_density/dicomWeb",
-      wadoRoot: "https://healthcare.googleapis.com/v1/projects/icad-med/locations/us-central1/datasets/mammo/dicomStores/breast_density/dicomWeb",
-      qidoSupportsIncludeField: !0,
-      imageRendering: "wadors",
-      thumbnailRendering: "wadors",
-      enableStudyLazyLoad: !0,
-      supportsFuzzyMatching: !0,
-      supportsWildcard: !1
-    }
-  }, {
-    friendlyName: "dicom json",
-    namespace: "@ohif/extension-default.dataSourcesModule.dicomjson",
-    sourceName: "dicomjson",
-    configuration: {
-      name: "json"
-    }
-  }, {
-    friendlyName: "dicom local",
-    namespace: "@ohif/extension-default.dataSourcesModule.dicomlocal",
-    sourceName: "dicomlocal",
-    configuration: {}
-  }],
-  httpErrorHandler: e => {
-    console.warn(e.status),
-      console.warn("test, navigate to https://ohif.org/")
-  }
-  ,
-  whiteLabeling: {
-    createLogoComponentFn: function (e) {
-      return e.createElement("a", {
-        target: "_self",
-        rel: "noopener noreferrer",
-        className: "text-purple-600 line-through",
-        href: "/"
-      }, e.createElement("img", {
-        src: "./assets/gradient.svg"
-      }))
-    }
-  },
-  defaultDataSourceName: "dicomweb",
-  hotkeys: [{
-    commandName: "incrementActiveViewport",
-    label: "Next Viewport",
-    keys: ["right"]
-  }, {
-    commandName: "decrementActiveViewport",
-    label: "Previous Viewport",
-    keys: ["left"]
-  }, {
-    commandName: "rotateViewportCW",
-    label: "Rotate Right",
-    keys: ["r"]
-  }, {
-    commandName: "rotateViewportCCW",
-    label: "Rotate Left",
-    keys: ["l"]
-  }, {
-    commandName: "invertViewport",
-    label: "Invert",
-    keys: ["i"]
-  }, {
-    commandName: "flipViewportHorizontal",
-    label: "Flip Horizontally",
-    keys: ["h"]
-  }, {
-    commandName: "flipViewportVertical",
-    label: "Flip Vertically",
-    keys: ["v"]
-  }, {
-    commandName: "scaleUpViewport",
-    label: "Zoom In",
-    keys: ["+"]
-  }, {
-    commandName: "scaleDownViewport",
-    label: "Zoom Out",
-    keys: ["-"]
-  }, {
-    commandName: "fitViewportToWindow",
-    label: "Zoom to Fit",
-    keys: ["="]
-  }, {
-    commandName: "resetViewport",
-    label: "Reset",
-    keys: ["space"]
-  }, {
-    commandName: "nextImage",
-    label: "Next Image",
-    keys: ["down"]
-  }, {
-    commandName: "previousImage",
-    label: "Previous Image",
-    keys: ["up"]
-  }, {
-    commandName: "setToolActive",
-    commandOptions: {
-      toolName: "Zoom"
+  oidc: [
+    {
+      authority: 'https://accounts.google.com',
+      client_id: '503845864048-iueskijtmfs09vsgk08r80nbj56s0gev.apps.googleusercontent.com',
+      redirect_uri: '/callback',
+      response_type: 'id_token token',
+      scope: [
+        'email',
+        'profile',
+        'openid',
+        'https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/devstorage.read_write',
+        'https://www.googleapis.com/auth/bigquery.readonly',
+        'https://www.googleapis.com/auth/drive.metadata.readonly',
+        'https://www.googleapis.com/auth/drive.readonly',
+        'https://www.googleapis.com/auth/cloudplatformprojects.readonly',
+        'https://www.googleapis.com/auth/cloud-healthcare',
+      ].join(' '),
+      post_logout_redirect_uri: '/logout-redirect.html',
+      revoke_uri: 'https://accounts.google.com/o/oauth2/revoke?token=',
+      automaticSilentRenew: true,
+      revokeAccessTokenOnSignout: true,
     },
-    label: "Zoom",
-    keys: ["z"]
-  }, {
-    commandName: "windowLevelPreset1",
-    label: "W/L Preset 1",
-    keys: ["1"]
-  }, {
-    commandName: "windowLevelPreset2",
-    label: "W/L Preset 2",
-    keys: ["2"]
-  }, {
-    commandName: "windowLevelPreset3",
-    label: "W/L Preset 3",
-    keys: ["3"]
-  }, {
-    commandName: "windowLevelPreset4",
-    label: "W/L Preset 4",
-    keys: ["4"]
-  }, {
-    commandName: "windowLevelPreset5",
-    label: "W/L Preset 5",
-    keys: ["5"]
-  }, {
-    commandName: "windowLevelPreset6",
-    label: "W/L Preset 6",
-    keys: ["6"]
-  }, {
-    commandName: "windowLevelPreset7",
-    label: "W/L Preset 7",
-    keys: ["7"]
-  }, {
-    commandName: "windowLevelPreset8",
-    label: "W/L Preset 8",
-    keys: ["8"]
-  }, {
-    commandName: "windowLevelPreset9",
-    label: "W/L Preset 9",
-    keys: ["9"]
-  }]
+  ],
+  // filterQueryParam: false,
+  dataSources: [
+    {
+      friendlyName: 'dcmjs DICOMWeb Server',
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
+      sourceName: 'dicomweb',
+      configuration: {
+        name: 'GCP',
+        wadoUriRoot:
+          'https://healthcare.googleapis.com/v1/projects/icad-med/locations/us-central1/datasets/mammo/dicomStores/breast_density/dicomWeb',
+        qidoRoot:
+          'https://healthcare.googleapis.com/v1/projects/icad-med/locations/us-central1/datasets/mammo/dicomStores/breast_density/dicomWeb',
+        wadoRoot:
+          'https://healthcare.googleapis.com/v1/projects/icad-med/locations/us-central1/datasets/mammo/dicomStores/breast_density/dicomWeb',
+        qidoSupportsIncludeField: !0,
+        imageRendering: 'wadors',
+        thumbnailRendering: 'wadors',
+        enableStudyLazyLoad: !0,
+        supportsFuzzyMatching: !0,
+        supportsWildcard: !1,
+        requestTransferSyntaxUID: '*',
+      },
+    },
+    {
+      friendlyName: 'dicom json',
+      namespace: '@gradienthealth/ohif-gradienthealth-extension.dataSourcesModule.bq',
+      sourceName: 'bq',
+      configuration: {
+        name: 'json',
+      },
+    },
+    {
+      friendlyName: 'dicom local',
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomlocal',
+      sourceName: 'dicomlocal',
+      configuration: {},
+    },
+  ],
+  httpErrorHandler: error => {
+    // This is 429 when rejected from the public idc sandbox too often.
+    console.warn(error.status);
+
+    // Could use services manager here to bring up a dialog/modal if needed.
+    console.warn('test, navigate to https://ohif.org/');
+  },
+  whiteLabeling: {
+    /* Optional: Should return a React component to be rendered in the "Logo" section of the application's Top Navigation bar */
+    createLogoComponentFn: function (React) {
+      return React.createElement(
+        'a',
+        {
+          target: '_self',
+          rel: 'noopener noreferrer',
+          className: 'text-purple-600 line-through',
+          href: '/',
+        },
+        React.createElement('img', {
+          src: '/assets/gradient.svg',
+        })
+      );
+    },
+  },
+  defaultDataSourceName: 'dicomweb',
+  hotkeys: [
+    {
+      commandName: 'incrementActiveViewport',
+      label: 'Next Viewport',
+      keys: ['right'],
+    },
+    {
+      commandName: 'decrementActiveViewport',
+      label: 'Previous Viewport',
+      keys: ['left'],
+    },
+    { commandName: 'rotateViewportCW', label: 'Rotate Right', keys: ['r'] },
+    { commandName: 'rotateViewportCCW', label: 'Rotate Left', keys: ['l'] },
+    { commandName: 'invertViewport', label: 'Invert', keys: ['i'] },
+    {
+      commandName: 'flipViewportHorizontal',
+      label: 'Flip Horizontally',
+      keys: ['h'],
+    },
+    {
+      commandName: 'flipViewportVertical',
+      label: 'Flip Vertically',
+      keys: ['v'],
+    },
+    { commandName: 'scaleUpViewport', label: 'Zoom In', keys: ['+'] },
+    { commandName: 'scaleDownViewport', label: 'Zoom Out', keys: ['-'] },
+    { commandName: 'fitViewportToWindow', label: 'Zoom to Fit', keys: ['='] },
+    { commandName: 'resetViewport', label: 'Reset', keys: ['space'] },
+    { commandName: 'nextImage', label: 'Next Image', keys: ['down'] },
+    { commandName: 'previousImage', label: 'Previous Image', keys: ['up'] },
+    // {
+    //   commandName: 'previousViewportDisplaySet',
+    //   label: 'Previous Series',
+    //   keys: ['pagedown'],
+    // },
+    // {
+    //   commandName: 'nextViewportDisplaySet',
+    //   label: 'Next Series',
+    //   keys: ['pageup'],
+    // },
+    {
+      commandName: 'setToolActive',
+      commandOptions: { toolName: 'Zoom' },
+      label: 'Zoom',
+      keys: ['z'],
+    },
+    // ~ Window level presets
+    {
+      commandName: 'windowLevelPreset1',
+      label: 'W/L Preset 1',
+      keys: ['1'],
+    },
+    {
+      commandName: 'windowLevelPreset2',
+      label: 'W/L Preset 2',
+      keys: ['2'],
+    },
+    {
+      commandName: 'windowLevelPreset3',
+      label: 'W/L Preset 3',
+      keys: ['3'],
+    },
+    {
+      commandName: 'windowLevelPreset4',
+      label: 'W/L Preset 4',
+      keys: ['4'],
+    },
+    {
+      commandName: 'windowLevelPreset5',
+      label: 'W/L Preset 5',
+      keys: ['5'],
+    },
+    {
+      commandName: 'windowLevelPreset6',
+      label: 'W/L Preset 6',
+      keys: ['6'],
+    },
+    {
+      commandName: 'windowLevelPreset7',
+      label: 'W/L Preset 7',
+      keys: ['7'],
+    },
+    {
+      commandName: 'windowLevelPreset8',
+      label: 'W/L Preset 8',
+      keys: ['8'],
+    },
+    {
+      commandName: 'windowLevelPreset9',
+      label: 'W/L Preset 9',
+      keys: ['9'],
+    },
+  ],
 };
