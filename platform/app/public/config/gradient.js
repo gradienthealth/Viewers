5e258360a81d9ce3bb39513b83e6c30dca5a96dd
Adithyan Dinesh <adithyan.dinesh@trenser.com>
2024-11-13 18:21:37 +0530

    Setting the useSharedArrayBuffer flag as auto

--- platform/app/public/config/gradient.js
+++ platform/app/public/config/gradient.js
@@ -10,7 +10,7 @@ window.config = {
   showStudyList: true,
   // some windows systems have issues with more than 3 web workers
   maxNumberOfWebWorkers: 3,
-
+  useSharedArrayBuffer: 'AUTO',
   // below flag is for performance reasons, but it might not work for all servers
   omitQuotationForMultipartRequest: true,
   showWarningMessageForCrossOrigin: false,
