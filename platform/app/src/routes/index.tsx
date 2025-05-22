45bd76eea190ff0c0db9ef12915dc5831709fa39
Adithyan Dinesh <adithyan.dinesh@trenser.com>
2025-05-22 18:15:29 +0530

    Provided bucket details and studyUIDs through url params and query params instead of an input field

--- platform/app/src/routes/index.tsx
+++ platform/app/src/routes/index.tsx
@@ -84,7 +84,7 @@ const bakedInRoutes = [
     children: Local.bind(null, { modePath: 'viewer/dicomlocal' }),
   },
   {
-    path: `/download`,
+    path: `/download/:bucket/*`,
     children: Download,
   },
 ];
