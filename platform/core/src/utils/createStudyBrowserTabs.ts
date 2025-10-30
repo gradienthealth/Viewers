8e92435b06591a79b2dbacd60e9f73b4cd3e05e3
Adithyan Dinesh <adithyan.dinesh@trenser.com>
2025-10-30 18:21:05 +0530

    Added series filter in COD study loading for non-deid buckets

--- platform/core/src/utils/createStudyBrowserTabs.ts
+++ platform/core/src/utils/createStudyBrowserTabs.ts
@@ -25,6 +25,8 @@ export function createStudyBrowserTabs(
   const dataSource = extensionManager.getActiveDataSource()[0];
   const dicomWebClient = dataSource.retrieve.getWadoDicomWebClient?.();
   const omittedSeries = dicomWebClient.getOmittedSeries?.() || [];
+  const queryParams = new URLSearchParams(window.location.search);
+  const seriesUIdsToFilter = queryParams.getAll('SeriesInstanceUIDs');
 
   const shouldSortBySeriesUID = process.env.TEST_ENV === 'true';
   const primaryStudies = [];
@@ -49,7 +51,11 @@ export function createStudyBrowserTabs(
     }
 
     omittedSeries.forEach(aOmittedSeries => {
-      if (aOmittedSeries.studyInstanceUID === study.studyInstanceUid) {
+      if (
+        aOmittedSeries.studyInstanceUID === study.studyInstanceUid &&
+        (!seriesUIdsToFilter.length ||
+          seriesUIdsToFilter.includes(aOmittedSeries.seriesInstanceUID))
+      ) {
         sortedDisplaySets.push({
           StudyInstanceUID: study.studyInstanceUid,
           SeriesInstanceUID: aOmittedSeries.seriesInstanceUID,
