292c055ee1f4a847bb0998950ab8403018138122
Adithyan Dinesh <148532903+Adithyan-Dinesh-Trenser@users.noreply.github.com>
2025-11-26 18:58:52 +0530

    Disabling the 'series filter failed' notification temporarily due to being out of context in some sheet-integrated scenarios

--- platform/app/src/routes/Mode/defaultRouteInit.ts
+++ platform/app/src/routes/Mode/defaultRouteInit.ts
@@ -61,6 +61,8 @@ export async function defaultRouteInit(
     function ({ StudyInstanceUID, SeriesInstanceUID, madeInClient = false }) {
       const seriesMetadata = DicomMetadataStore.getSeries(StudyInstanceUID, SeriesInstanceUID);
 
+      /* The 'series filter failed' notification is out of context in
+      some sheet-integrated scenarios, so disabling it temporarily.
       // checks if the series filter was used, if it exists
       const seriesInstanceUIDs = filters?.seriesInstanceUID;
       if (
@@ -77,6 +79,7 @@ export async function defaultRouteInit(
           duration: 7000,
         });
       }
+      */
 
       displaySetService.makeDisplaySets(seriesMetadata.instances, { madeInClient });
     }
