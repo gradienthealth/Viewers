239a21e57e79af69e9731606575eacfa5b299d42
Adithyan Dinesh <adithyan.dinesh@trenser.com>
2025-12-23 18:58:28 +0530

    OPFS management tool improvements

--- extensions/cornerstone/src/components/OPFSManagementTool/constants.ts
+++ extensions/cornerstone/src/components/OPFSManagementTool/constants.ts
@@ -1,2 +1,29 @@
 export const OPFS_VERSION_STORAGE_KEY = 'gh-opfs-path-version';
 export const CURRENT_OPFS_VERSION = 1;
+
+export const OPFS_PURGE_METADATA = [
+  {
+    label: 'All',
+    time: null,
+  },
+  {
+    label: '1 Hour',
+    time: 1000 * 60 * 60,
+  },
+  {
+    label: '4 Hours',
+    time: 1000 * 60 * 60 * 4,
+  },
+  {
+    label: '12 Hours',
+    time: 1000 * 60 * 60 * 12,
+  },
+  {
+    label: '1 Day',
+    time: 1000 * 60 * 60 * 24,
+  },
+  {
+    label: '1 Week',
+    time: 1000 * 60 * 60 * 24 * 7,
+  },
+];
