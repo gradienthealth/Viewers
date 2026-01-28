3fff151dbab49f9fa3f85102af7f57a281aadd5d
Adithyan Dinesh <148532903+Adithyan-Dinesh-Trenser@users.noreply.github.com>
2026-01-28 18:44:17 +0530

    Fixed the series filter issue in non-deid buckets

--- platform/core/src/utils/createStudyBrowserTabs.ts
+++ platform/core/src/utils/createStudyBrowserTabs.ts
@@ -36,7 +36,7 @@ export function createStudyBrowserTabs(
     const displaySetsForStudy = displaySets.filter(
       ds =>
         ds.StudyInstanceUID === study.studyInstanceUid &&
-        (!seriesUIdsToFilter.length || seriesUIdsToFilter.includes(ds.SeriesInstanceUID))
+        (!seriesUIdsToFilter.length || seriesUIdsToFilter.includes(ds.DeidSeriesInstanceUID))
     );
 
     // sort them by seriesInstanceUID
