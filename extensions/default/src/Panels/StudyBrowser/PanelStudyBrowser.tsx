9b0358d3b05b5d9c425ef347c454b6819bf6b48e
Adithyan Dinesh <adithyan.dinesh@trenser.com>
2026-01-29 16:00:20 +0530

    Fixed the series filter issue in some studies in the segmentation mode

--- extensions/default/src/Panels/StudyBrowser/PanelStudyBrowser.tsx
+++ extensions/default/src/Panels/StudyBrowser/PanelStudyBrowser.tsx
@@ -536,7 +536,7 @@ function _mapDisplaySets(displaySets, displaySetLoadingState, thumbnailImageSrcM
         countIcon: ds.countIcon,
         messages: ds.messages,
         StudyInstanceUID: ds.StudyInstanceUID,
-        SeriesInstanceUID: ds.SeriesInstanceUID,
+        DeidSeriesInstanceUID: ds.instance.DeidSeriesInstanceUID,
         componentType,
         imageSrc: thumbnailSrc || thumbnailImageSrcMap[displaySetInstanceUID],
         dragData: {
